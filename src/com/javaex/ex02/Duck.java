10ce66773d754560b93e774545bf5570d775cda0
H-JUYEONG <dud9902@gmail.com>
2024-07-09 17:04:45 +0900

    1~3

---
+++ src/com/javaex/ex02/Duck.java
@@ -0,0 +1,31 @@
+package com.javaex.ex02;
+
+public class Duck extends Bird {
+
+	private String name;
+
+	public Duck() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void fly() {
+		System.out.println("오리(" + name + ")가 날지 않습니다.");
+	}
+	
+	public void sing() {
+		System.out.println("오리(" + name + ")가 소리내어 웁니다.");
+	}
+
+	public void showName() {
+		System.out.println("오리의 이름은 " + name + " 입니다.");
+	}
+
+}
